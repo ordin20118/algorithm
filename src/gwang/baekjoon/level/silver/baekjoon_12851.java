c91a7e77780549dd4aee7b1dcb07808b1c46e499
ordin20118 <75382892+ordin20118@users.noreply.github.com>
2022-08-16 14:38:44 +0900

    baekjoon exam 12851

--- src/gwang/baekjoon/level/silver/baekjoon_12851.java
+++ src/gwang/baekjoon/level/silver/baekjoon_12851.java
@@ -1,6 +1,6 @@
 package gwang.baekjoon.level.silver;
 
-
+import java.util.*;
 
 public class baekjoon_12851 {
 
@@ -22,22 +22,102 @@ public class baekjoon_12851 {
 	 * 첫째 줄에 수빈이가 동생을 찾는 가장 빠른 시간을 출력한다.
 	 * 둘째 줄에는 가장 빠른 시간으로 수빈이가 동생을 찾는 방법의 수를 출력한다.
 	 * 
+	 * 문제를 다 푼거 같은데도 틀렸을때는 주어진 입력이 예상밖의 경우(N과 K가 이미 같은)를 생각해본다.
+	 * 
 	 */
 	
 	
+	static int N;
+	static int K;
+	
+	static Step[] visited;
 	
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
+		N = sc.nextInt();
+		K = sc.nextInt();
+		
+		visited = new Step[100001];
+		
+		bfs();
+		
+		System.out.println(visited[K].time);
+		System.out.println(visited[K].cnt);
+
+	}
+
+	public static void bfs() {
+
+		Queue<Step> queue = new LinkedList<>();
+		
+		// 첫번재 위치 삽입
+		Step first = new Step(N, 0, 1);
+		queue.add(first);
+		visited[N] = first;
+		
+		while(!queue.isEmpty()) {
+			
+			Step now = queue.poll();
+			
+			// 방문 확인
+			if(visited[now.position] != null) {
+				if(visited[now.position].time > now.time) {
+					visited[now.position] = now;
+				} else if(visited[now.position].time == now.time) {
+					
+					if(visited[now.position].time != 0) {
+						visited[now.position].cnt++;	
+					}
+										
+				} else {
+					continue;
+				}
+			} else {
+				visited[now.position] = now;
+			}
+			
+			// 목적지 도달
+			if(now.position == K) {
+				continue;
+			}
+			
+			if(visited[K] != null && now.time >= visited[K].time) {
+				continue;
+			}
+			
+			// -1 => action 0
+			if(now.position != 0) {
+				Step next = new Step(now.position - 1, now.time + 1, 1);
+				visited[N] = next;
+				queue.add(next);
+			}
+			
+			// +1 => action 1
+			if(now.position < 100000) {
+				Step next = new Step(now.position + 1, now.time + 1, 1);
+				visited[N] = next;
+				queue.add(next);				
+			}
+			
+			// *2 => action 2
+			if(now.position * 2 <= 100000 && now.position != 0) {
+				Step next = new Step(now.position * 2, now.time + 1, 1);
+				visited[N] = next;
+				queue.add(next);				
+			}
+		}
 		
 	}
 	
-	public static void find() {
-		
-		
-				
+	static class Step {
+		int position;
+		int time;
+		int cnt;
+		Step (int position, int time, int cnt) {
+			this.position = position; this.time = time; this.cnt = cnt;
+		}
 	}
-	
-	
 
 }
